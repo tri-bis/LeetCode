702db51b7799592bf6634c220c609e541d302214
tri-bis <167125214+tri-bis@users.noreply.github.com>
2026-07-14 23:57:32 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.4%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,9 +4,23 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         for(int i =0;i<m;i++){
-            for(int j =0; j<n;j++){
-                if(matrix[i][j] == target){
+            // for(int j =0; j<n;j++){
+            //     if(matrix[i][j] == target){
+            //         return true;
+            //     }
+            // }
+            int left = 0;
+            int right = n-1;
+            while(left <= right){
+                int mid = (left + right)/2;
+                if(matrix[i][mid] == target){
                     return true;
+                }
+                else if(matrix[i][mid] < target){
+                    left = mid+1;
+                }
+                else{
+                    right=mid-1;
                 }
             }
         }
