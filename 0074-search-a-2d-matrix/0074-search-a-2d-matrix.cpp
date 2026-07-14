fa92bfc658d6ff6c4e773d77565a5acc7b9fd880
tri-bis <167125214+tri-bis@users.noreply.github.com>
2026-07-15 00:22:19 +0530

    Time: 0 ms (100%), Space: 13.3 MB (79.18%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,25 +3,38 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
-        for(int i =0;i<m;i++){
-            // for(int j =0; j<n;j++){
-            //     if(matrix[i][j] == target){
-            //         return true;
-            //     }
-            // }
-            int left = 0;
-            int right = n-1;
-            while(left <= right){
-                int mid = (left + right)/2;
-                if(matrix[i][mid] == target){
-                    return true;
-                }
-                else if(matrix[i][mid] < target){
-                    left = mid+1;
-                }
-                else{
-                    right=mid-1;
-                }
+        // for(int i =0;i<m;i++){
+            
+        //     int left = 0;
+        //     int right = n-1;
+        //     while(left <= right){
+        //         int mid = (left + right)/2;
+        //         if(matrix[i][mid] == target){
+        //             return true;
+        //         }
+        //         else if(matrix[i][mid] < target){
+        //             left = mid+1;
+        //         }
+        //         else{
+        //             right=mid-1;
+        //         }
+        //     }
+        // }
+        // return false;
+
+
+
+        int col = n-1;
+        int row =0;
+        while(row<m && col>=0){
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] > target){
+                col--;
+            }
+            else{
+                row++;
             }
         }
         return false;
