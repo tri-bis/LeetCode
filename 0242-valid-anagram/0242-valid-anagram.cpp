651c0af151be869ff73dad9526cfc6e012e1d4b7
Trisha Biswas <167125214+tri-bis@users.noreply.github.com>
2026-07-20 00:04:17 +0530

    Time: 4 ms (34.71%), Space: 9.8 MB (50.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,37 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        cout << s << " " << t;
-        if (s != t) {
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // cout << s << " " << t;
+        // if (s != t) {
+        //     return false;
+        // }
+        // return true;
+
+
+        if(s.length() == t.length()){
+            vector<int>count(26);
+            int i = 0;
+            while(i< s.length()){
+                int idx = s[i] - 'a';
+                count[idx]++;
+                i++; 
+            }
+            int j =0;
+            while(j< t.length()){
+                int idx = t[j] - 'a';
+                if(count[idx] == 0){
+                    return false;
+                }
+                else{
+                    count[idx]--;
+                }
+              j++;   
+            }
+        }else{
             return false;
         }
-        return true;
+    return true;
     }
 };
