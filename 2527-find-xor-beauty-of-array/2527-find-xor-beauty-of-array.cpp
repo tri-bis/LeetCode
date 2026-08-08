3ffa98ad47667bac307fd977782191a6ce7316e5
Trisha Biswas <167125214+tri-bis@users.noreply.github.com>
2026-08-08 22:09:18 +0530

    Time: 0 ms (100%), Space: 43.8 MB (6.44%) - LeetHub

---
+++ 2527-find-xor-beauty-of-array/2527-find-xor-beauty-of-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int xorBeauty(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i = 0 ; i < n;i++){
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+};
